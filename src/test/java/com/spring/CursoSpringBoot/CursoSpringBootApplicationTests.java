6813506c6f41dd8d64e40dc3ccc462bf9e06b94e
Mateus Vitor <stanniack@gmail.com>
2020-06-12 02:01:33 -0300

    First commit - setup project

---
+++ src/test/java/com/spring/CursoSpringBoot/CursoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.CursoSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
