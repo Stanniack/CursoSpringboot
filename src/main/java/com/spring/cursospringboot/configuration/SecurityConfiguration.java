507906b62f19cecac4f8758e2624463b8d73d43d
Mateus Vitor <stanniack@gmail.com>
2020-06-19 10:05:04 -0300

    Atualização editpost e Spring Security
    Spring security ainda está incompleto.

---
+++ src/main/java/com/spring/cursospringboot/configuration/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.spring.cursospringboot.configuration;
+
+/*import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;*/
+
+
+public class SecurityConfiguration /*extends WebSecurityConfigurerAdapter*/ {
+
+//    /* Habilita os recursos de segurança */
+//    @EnableWebSecurity
+//    public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
+//
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http.authorizeRequests()
+//                    .anyRequest().authenticated() // Para qualquer requisição é preciso estar autenticado
+//                    .and()
+//                    .httpBasic();
+//        }
+//
+//    }
+//
+//    /* Autenticação de usuário */
+//    @Override
+//    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+//        builder
+//                .inMemoryAuthentication()
+//                .withUser("garrincha").password("{noop}123")
+//                .roles("USER")
+//                .and()
+//                .withUser("zico").password("123")
+//                .roles("USER");
+//    }
+
+}
+
